c1a96d3d792a85c74e4fc2c316d0e8accd5ef3a4
Saad AlEssa <saadalessa@Saads-MacBook-Pro.local>
2018-06-12 23:48:34 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engin','hbs');
+
+app.use('/',(req, res, next) => {
+    res.render('maintenance.hbs');
+}),
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+})
+
+
+app.listen(3000);
